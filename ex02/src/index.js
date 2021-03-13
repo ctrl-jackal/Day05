137f8d729d452722c9119ac898d4e9b3b442115e
Maid <maidsivac@gmail.com>
2021-03-13 01:06:32 +0100

    Adding tasks of day05

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    var myLink = '<a href="https://www.example.com" target="_blank">Link</a>'
+    
+    // Only change code above this line
+    return myLink;
+
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
